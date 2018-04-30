1d0719488eef8a1876b09e1b826934fdb5cc7ce9
Calvin <kclamap@connect.ust.hk>
2018-05-01 03:23:30 +0800

    Update BarChart_.java
    BarChart Extends from Chart

--- src/core/comp3111/BarChart_.java
+++ src/core/comp3111/BarChart_.java
@@ -2,58 +2,56 @@ package core.comp3111;
 
 import java.util.Set;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
+import javafx.geometry.Pos;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.chart.BarChart;
 
-public class BarChart_ extends Application {
-	private DataTable generateDummyDataTable() throws DataTableException {
-		DataTable t = new DataTable();
-
-        String[] Year = new String[] {"2003", "2004", "2005"};
-        DataColumn YearCol = new DataColumn(DataType.TYPE_STRING, Year);
-        
-        
-		Number[] austria = new Float[] { (float) 25601, (float) 57401, (float) 45000};
-		DataColumn austriaCol = new DataColumn(DataType.TYPE_NUMBER, austria);
-		
-		Number[] brazil = new Float[] { (float) 20148, (float) 41941, (float) 44835};
-		DataColumn brazilCol = new DataColumn(DataType.TYPE_NUMBER, brazil);
-		
-		Number[] france = new Float[] { (float) 10000, (float) 45263, (float) 18722};
-		DataColumn franceCol = new DataColumn(DataType.TYPE_NUMBER, france);
-		
-		Number[] italy = new Float[] { (float) 35407, (float) 117320, (float) 17557};
-		DataColumn italyCol = new DataColumn(DataType.TYPE_NUMBER, italy);
-		
-		Number[] usa = new Float[] { (float) 12000, (float) 14845, (float) 92633};
-		DataColumn usaCol = new DataColumn(DataType.TYPE_NUMBER, usa);
-				
-
-		t.addCol("Year", YearCol);
-		t.addCol("Austria", austriaCol);
-		t.addCol("Brazil", brazilCol);
-		t.addCol("France", franceCol);
-		t.addCol("Italy", italyCol);
-		t.addCol("USA", usaCol);
-		
-		return t;
+public class BarChart_ extends Chart {
+	
+	private static CategoryAxis xAxis = null;
+	private static NumberAxis yAxis = null;
+    private static BarChart<String,Number> bc = null;
+	
+	public BarChart_(DataTable t) {
+		xAxis = new CategoryAxis();
+		yAxis = new NumberAxis();
+		bc = new BarChart<String,Number>(xAxis,yAxis);
+		setDataset(t);
+		setType(0);
 	}
 	
-	public static BarChart generateBarChart(DataTable t) {
-		//stage.setTitle("Bar Chart Sample");
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        final BarChart<String,Number> bc = new BarChart<String,Number>(xAxis,yAxis);
-        bc.setTitle("Country Summary");
-        xAxis.setLabel("Country");       
-        yAxis.setLabel("Value");
-        
-        DataTable dataset = t;
+	private Pane paneBarChartScreen(String xAxisLabel, String yAxisLabel, String chartTitle) {
+		btLineChartBackMain = new Button("Back");
+
+		xAxis.setLabel(xAxisLabel);
+		yAxis.setLabel(yAxisLabel);
+		bc.setTitle(chartTitle);
+
+		// Layout the UI components
+		VBox container = new VBox(20);
+		container.getChildren().addAll(bc, btLineChartBackMain);
+		container.setAlignment(Pos.CENTER);
+
+		BorderPane pane = new BorderPane();
+		pane.setCenter(container);
+
+		// Apply CSS to style the GUI components
+		pane.getStyleClass().add("screen-background");
+
+		return pane;	
+	}
+	
+	public Pane paneChart(String xAxisLabel, String yAxisLabel, String chartTitle) {
+		return paneBarChartScreen(xAxisLabel, yAxisLabel, chartTitle);
+	}
+	
+	private void populateDataToBarChart() {
     	int numKey = dataset.getNumCol();
     	int numTextCol = 1;
     	int numNumericCol = numKey - numTextCol;
@@ -62,6 +60,7 @@ public class BarChart_ extends Application {
     	String[] textCol = new String[rowSize];
     	String[] keyRow = new String[numKey];
     	Float[][] data = new Float[numNumericCol][rowSize];
+    	
     	int j = 1;
     	Set<String> keys = dataset.getDC().keySet();
     	for (String key : keys) {
@@ -95,24 +94,44 @@ public class BarChart_ extends Application {
     	for (int i = 0; i < rowSize ; i++) {
     		bc.getData().add(series[i]);
     	}
-    	
-    	return bc;
-    	
 	}
-
-	@Override public void start(Stage stage) throws DataTableException {
-        
-        /////////////////////////////////////////////////////////////////
-        DataTable t = generateDummyDataTable();
-    	
-        Scene scene  = new Scene(generateBarChart(t),800,600);
-
-        stage.setScene(scene);
-        stage.show();
-    	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public void populateDataToChart(){
+		populateDataToBarChart();
 	}
 
+	private DataTable generateDummyDataTable() throws DataTableException {
+
+		DataTable t = new DataTable();
+
+        String[] Year = new String[] {"2003", "2004", "2005"};
+        DataColumn YearCol = new DataColumn(DataType.TYPE_STRING, Year);
+        
+        
+		Number[] austria = new Float[] { (float) 25601, (float) 57401, (float) 45000};
+		DataColumn austriaCol = new DataColumn(DataType.TYPE_NUMBER, austria);
+		
+		Number[] brazil = new Float[] { (float) 20148, (float) 41941, (float) 44835};
+		DataColumn brazilCol = new DataColumn(DataType.TYPE_NUMBER, brazil);
+		
+		Number[] france = new Float[] { (float) 10000, (float) 45263, (float) 18722};
+		DataColumn franceCol = new DataColumn(DataType.TYPE_NUMBER, france);
+		
+		Number[] italy = new Float[] { (float) 35407, (float) 117320, (float) 17557};
+		DataColumn italyCol = new DataColumn(DataType.TYPE_NUMBER, italy);
+		
+		Number[] usa = new Float[] { (float) 12000, (float) 14845, (float) 92633};
+		DataColumn usaCol = new DataColumn(DataType.TYPE_NUMBER, usa);
+				
+
+		t.addCol("Year", YearCol);
+		t.addCol("Austria", austriaCol);
+		t.addCol("Brazil", brazilCol);
+		t.addCol("France", franceCol);
+		t.addCol("Italy", italyCol);
+		t.addCol("USA", usaCol);
+		
+		return t;
+	}
 }
+
