7508604f367836bcfaa9a1f846c43587e36a06dd
Calvin <kclamap@connect.ust.hk>
2018-05-01 03:24:44 +0800

    Add files via upload
    Superclass Chart

---
+++ src/core/comp3111/Chart.java
@@ -0,0 +1,22 @@
+package core.comp3111;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public abstract class Chart {
+	private String[] typeSet = {"BarChart", "ScatterChart", "AnimatedChart"};
+	private int type = -1;
+	protected DataTable dataset;
+	protected Button btLineChartBackMain = null;
+	
+	abstract Pane paneChart(String xAxisLabel, String yAxisLabel, String chartTitle);
+	
+	abstract void populateDataToChart();
+	
+	public DataTable getDataset() {return dataset;}
+	public void setDataset(DataTable t) {dataset = t;}
+	
+	public String getType() {return typeSet[type];}
+	public void setType(int index) {type = index;}
+	
+}
