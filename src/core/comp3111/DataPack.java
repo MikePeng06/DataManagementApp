b14030996a5b7d6b0c67ad4f8e2ee31559ac242c
zpeng <panchiksen@gmail.com>
2018-05-03 17:54:14 +0800

    merge

--- src/core/comp3111/DataPack.java
+++ src/core/comp3111/DataPack.java
@@ -8,14 +8,14 @@ public class DataPack implements java.io.Serializable{
 	public ArrayList<DataTable> chartList = new ArrayList<DataTable>();
 	public ArrayList<String> dataTableName = new ArrayList<String>();
 	public ArrayList<String> charName = new ArrayList<String>();
-//	public ArrayList<DataTable> Chartdatatable = new ArrayList<DataTable>();
-//	public  ArrayList<String> ChartType = new ArrayList<String>();
-//	
-	public DataPack(ArrayList<DataTable> dtl, ArrayList<DataTable> ctl, ArrayList<String> dtn, ArrayList<String> cn) {
+	public ArrayList<DataTableArray> DTALIST = new ArrayList<DataTableArray>();
+	
+	public DataPack(ArrayList<DataTable> dtl, ArrayList<DataTable> ctl, ArrayList<String> dtn, ArrayList<String> cn, ArrayList<DataTableArray> dl) {
 		dataTableList = dtl;
 		chartList = ctl;
 		dataTableName = dtn;
 		charName = cn;
+		DTALIST = dl;
 //		Chartdatatable = cdt;
 //		ChartType = ct;
 	}
