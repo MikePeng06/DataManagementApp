0584d3df5f51cbc8b9616e34a26d01be0efd2c95
kclamap <kclamap@csl1wk10.win2k.cs.ust.hk>
2018-05-04 21:14:15 +0800

    included javaDoc

--- src/core/comp3111/BarChart_UI.java
+++ src/core/comp3111/BarChart_UI.java
@@ -13,12 +13,25 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * BarChart_: a class for constructing a bar chart
+ * it contains all the data members from JavaFX and methods (which involves use of JavaFX elements) needed for this purpose.
+ * @author kclamap
+ *
+ */
 public class BarChart_UI {
+	/**
+	 * data members of BarChart_UI
+	 */
 	private static CategoryAxis xAxis;
 	private static NumberAxis yAxis;
 	public BarChart<String,Number> bc;
 	public Button btLineChartBackMain;
 	
+	/**
+	 * Default constuctor of BarChart_UI
+	 * it initialize data members of BarChart_UI to meaningless values
+	 */
 	public BarChart_UI() {
 		
 		xAxis = new CategoryAxis();
@@ -27,6 +40,10 @@ public class BarChart_UI {
 		btLineChartBackMain = new Button();
 	}
 	
+	/**
+	 * it accepts a DataTableAaary and adds its content to a barChart for constructing it
+	 * @param dta
+	 */
 	public void populateDataToBarChartUI(DataTableArray dta) {
 	bc.getData().clear();
     	XYChart.Series [] series = new XYChart.Series[dta.rowSize];
@@ -49,6 +66,14 @@ public class BarChart_UI {
     	
     	
 	}
+	
+	/**
+	 * it takes in the x Axis' and y Axis' label as well as the title for the chart to initialize the pane containing the bar chart
+	 * @param xAxisLabel
+	 * @param yAxisLabel
+	 * @param chartTitle
+	 * @return
+	 */
 	public Pane paneBarChartScreen(String xAxisLabel, String yAxisLabel, String chartTitle) {
 		Button btLineChartBackMain = this.btLineChartBackMain;
 
@@ -70,18 +95,43 @@ public class BarChart_UI {
 		return pane;	
 	}
 	
+	/**
+	 * getter
+	 * @return
+ 	 *		data member xAxis
+	 */
 	public CategoryAxis getXAxis() {
 		return xAxis;
 	}
+	/**
+	 * getter
+	 * @return
+	 * 		data member yAxis
+	 */
 	public NumberAxis getYAxis() {
 		return yAxis;
 	}
+	/**
+	 * getter
+	 * @return
+	 * 		data member bc
+	 */		
 	public BarChart<String, Number> getBC(){
 		return bc;
 	}
+	/**
+	 * getter
+	 * @return
+	 * 		data member btLineChartBackMain
+	 */
 	public Button getBackButton() {
 		return btLineChartBackMain;
 	}
+	/**
+	 * setter
+	 * @param b
+	 * 		set data member btLineChartBackMain to b
+	 */		
 	public void setBackButton(Button b) {
 		btLineChartBackMain = b;
 	}
